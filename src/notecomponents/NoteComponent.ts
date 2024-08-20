14bc63fd94b25ed8d21837d2e69f9cef1f5b8d1c
James Coyle <jamescoyle1989@gmail.com>
2024-08-20 08:42:24 -0400

    More moving of note component stuff

--- src/notecomponents/NoteComponent.ts
+++ src/notecomponents/NoteComponent.ts
@@ -1,4 +1,5 @@
 import Note from '../models/Note';
+import { Notu } from '../services/Notu';
 
 /** The base interface that all note components must implement */
 export interface NoteComponent {
@@ -40,4 +41,58 @@ export interface NoteComponentProcessor {
         previous: NoteComponentInfo,
         next: NoteComponentInfo
     ) => NoteComponent;
+}
+
+
+/** Takes a note and a set of processors, returns an array of all the note components which make up that note's text */
+export function splitNoteTextIntoComponents(
+    note: Note,
+    notu: Notu,
+    componentProcessors: Array<NoteComponentProcessor>,
+    defaultProcessor: NoteComponentProcessor
+): Array<NoteComponent> {
+    
+    const componentInfos = recursiveSplitNoteText(note.text, note, componentProcessors, defaultProcessor);
+
+    const components: Array<NoteComponent> = [];
+    async function save(): Promise<void> {
+        note.text = components.map(x => x.getText()).join('');
+        await notu.saveNotes([note]);
+    }
+
+    let previous: NoteComponentInfo = null;
+    let current: NoteComponentInfo = componentInfos[0] ?? null;
+    let next: NoteComponentInfo = componentInfos[1] ?? null;
+    for (let i = 0; i < componentInfos.length; i++) {
+        components.push(current.processor.creator(current, note, save, previous, next));
+        previous = current;
+        current = next;
+        next = componentInfos[i + 2] ?? null;
+    }
+
+    return components;
+}
+
+function recursiveSplitNoteText(
+    text: string,
+    note: Note,
+    componentProcessors: Array<NoteComponentProcessor>,
+    defaultProcessor: NoteComponentProcessor
+): Array<NoteComponentInfo> {
+    let componentInfo: NoteComponentInfo = null;
+    for (const processor of componentProcessors) {
+        componentInfo = processor.identify(text);
+        if (!!componentInfo)
+            break;
+    }
+    if (!componentInfo)
+        return [new NoteComponentInfo(text, 0, defaultProcessor)];
+
+    const output: Array<NoteComponentInfo> = [];
+    if (componentInfo.start > 0)
+        output.push(...recursiveSplitNoteText(text.substring(0, componentInfo.start), note, componentProcessors, defaultProcessor));
+    output.push(componentInfo);
+    if (componentInfo.end < text.length)
+        output.push(...recursiveSplitNoteText(text.substring(componentInfo.end), note, componentProcessors, defaultProcessor));
+    return output;
 }
